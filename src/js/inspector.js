822b556174e5a70d6acaaa08279f43a01451f919
Istvan Kocsis <k.istvanxxx@gmail.com>
2020-08-11 23:53:44 +0200

    Update to prevent the latest guard

--- src/js/inspector.js
+++ src/js/inspector.js
@@ -1,6 +1,13 @@
 export class Inspector {
     getGuard() {
-        const sections = document.querySelectorAll('body section');
-        return sections[sections.length - 1];
+        const divs = document.querySelectorAll('body > div');
+
+        for (let i = 0; i < divs.length; i++) {
+            if (divs[i].querySelector("img[src='https://totalcar.hu/assets/images/common/adblock/totalcar/modal.jpg']") !== null) {
+                return divs[i];
+            }
+        }
+
+        return null;
     }
 }
